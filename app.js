6fb5d13ef495f3e4edc0c11e0c109006f6d447c6
Satyam Sonker <satyamsonker@Satyams-MacBook-Pro.local>
2022-01-16 02:08:58 +0530

    Add Environment Vars and gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express= require("express");
 const bodyParser = require("body-parser");
 const  ejs = require("ejs");
@@ -16,9 +17,10 @@ const  userSchema = new mongoose.Schema({
     email : String,
     password : String
 });
+
 // for encryption 
-const secret = "Thissourlittlesecret.";
-userSchema.plugin(encrypt,{secret : secret,encryptedFields: ['password']});
+
+userSchema.plugin(encrypt,{secret :process.env.SECRET,encryptedFields: ['password']});
 
 const User = new mongoose.model("User",userSchema);
 
