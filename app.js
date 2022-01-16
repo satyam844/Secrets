0cb06e4d596baa154d7dc5971d9315de59db960c
Satyam Sonker <satyamsonker@Satyams-MacBook-Pro.local>
2022-01-17 00:58:50 +0530

    Complete Secret App

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const  userSchema = new mongoose.Schema({
     email : String,
     password : String,
-    googleId : String
+    googleId : String,
+    secret: String
 });
 
 // for encryption 
@@ -86,12 +87,41 @@ app.get("/register",function(req,res){
     res.render("register");
 })
 app.get("/secrets",function(req,res){
+    User.find({"secret" : {$ne : null}},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                res.render("secrets", {usersWithSecrets : foundUser }); 
+            }
+        }
+    })
+})
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
-        res.redirect("/login");
+        res.redirect ("/login");
     }
+})
+app.post("/submit",function(req,res){
+    const submittedSeceret = req.body.secret;
+    User.findById(req.user.id , function(err,foundUser){
+       if(err){
+           console.log(err);
+       }
+       else{
+           if(foundUser){
+               foundUser.secret = submittedSeceret;
+               foundUser.save(function(){
+                   res.redirect("/secrets");
+               })
+           }
+       }
+    })
 })
 
 app.get("/logout",function(req,res){
